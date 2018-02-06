e9fa47982e2bfbef34ceb4e06caece318b03ba38
zelra <pierre.gilly@viacesi.fr>
2018-02-07 00:23:38 +0100

    Final A1

--- Main.py
+++ Main.py
@@ -2,4 +2,25 @@ import numpy as np
 import cv2
 
 def A1() :
+    # Load an color image in grayscale
+    img = cv2.imread('images/A1.pbm',0)
 
+    #img = img + 100
+
+    isize = img.shape
+    maxi=0
+    coord = []
+    for i in range(0, isize[0]):
+        for j in range (0, isize[1]):
+            if img[i][j]>maxi:
+                maxi=img[i][j]
+                coord = [i, j]
+
+    print(coord)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+A1()
