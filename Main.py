a32b1820b1e26b03967460b6d9308ebb58fdd51d
zelra <pierre.gilly@viacesi.fr>
2018-02-07 14:06:05 +0100

    A3 final

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+from numpy import vectorize
+from numpy.lib.tests.test__datasource import valid_baseurl
+
 
 def A1() :
 
@@ -48,6 +51,18 @@ def A2() :
 
 def A3() :
 
+    img = cv2.imread('images/A3.pbm', 0)
+    isize = img.shape
 
+    for i in range(0, isize[0]):
+        for j in range (0, isize[1]):
+                if img[i][j] > 180 :
+                    img[i][j] = 0
+                else :
+                    img[i][j] = 255
+                
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 A3()
