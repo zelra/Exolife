b1105dd18598b9d7f1e8696e2181702722ec4f90
zelra <pierre.gilly@viacesi.fr>
2018-02-14 10:48:46 +0100

    U1 final

--- Main.py
+++ Main.py
@@ -200,4 +200,26 @@ def X2() :
 
 def U1() :
 
+    img = cv2.imread('images/U1.pbm', 0)
+
+  #  imgG = cv2.GaussianBlur(img, (3, 3), 0)
+
+    laplacian = cv2.Laplacian(img, cv2.CV_64F)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    sobelxy = cv2.Sobel(img, cv2.CV_64F, 1, 1, ksize=3)
+
+    plt.subplot(2, 5, 1), plt.imshow(img, cmap='gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 5, 2), plt.imshow(laplacian, cmap='gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 5, 3), plt.imshow(sobelx, cmap='gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 5, 4), plt.imshow(sobely, cmap='gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 5, 5), plt.imshow(sobelxy, cmap='gray')
+    plt.title('Sobel XY'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
 U1()
